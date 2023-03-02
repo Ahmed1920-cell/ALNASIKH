edb8d416b1d7327968f4d7edd9ddd6978096dcb1
Ahmed1920-cell <70626826+Ahmed1920-cell@users.noreply.github.com>
2023-03-02 19:50:03 +0200

    ffff

--- untitled/lib/imageview.dart
+++ untitled/lib/imageview.dart
@@ -12,24 +12,29 @@ import 'package:syncfusion_flutter_pdf/pdf.dart';
 import 'package:open_file/open_file.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:dio/dio.dart';
+
 class imageView extends StatefulWidget {
-  imageView({super.key, required this.imagePath,required this.ip});
+  imageView({super.key, required this.imagePath, required this.ip});
+
   final imagePath;
   final ip;
+
   @override
-  State<imageView> createState() => _imageViewState(imagePath,ip);
+  State<imageView> createState() => _imageViewState(imagePath, ip);
 }
 
 class _imageViewState extends State<imageView> {
+  _imageViewState(this.imageFile, this.IP);
 
-  _imageViewState(this.imageFile,this.IP);
   final IP;
-  var chr="";
-  int n=0;
+  var chr = "";
+  int n = 0;
   final imageFile;
+  bool NotCopy = true;
+
   ocr() async {
     var stream =
-    new http.ByteStream(DelegatingStream.typed(imageFile.openRead()));
+        new http.ByteStream(DelegatingStream.typed(imageFile.openRead()));
     // get file length
     var length = await imageFile.length();
 
@@ -52,78 +57,96 @@ class _imageViewState extends State<imageView> {
 
     // listen for response
     response.stream.transform(utf8.decoder).listen((value) async {
-      var str="";
+      var str = "";
       for (int i = 0; i < int.parse(value); i++) {
         final res =
-        await http.get(Uri.parse("http://$IP:5000/to_ocr?number=$i"));
+            await http.get(Uri.parse("http://$IP:5000/to_ocr?number=$i"));
         if (res.statusCode == 200) {
-          str=str+res.body;
+          str = str + res.body;
           setState(() {
-            chr=str;
+            chr = str;
           });
         }
       }
+      setState(() {
+        NotCopy = false;
+      });
       print(str);
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    if(n==0)
-      ocr();
+    if (n == 0) ocr();
     n++;
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('image view'),
-        ),
-        body: Container(
-          color: Colors.black,
-          child: Center(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('image view'),
+          ),
+          body: Container(
+            color: Colors.black,
+            child: Center(
               child: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    Image.file(
-                      imageFile,
-                      width: 400,
-                      height: 400,
+                child: Column(children: [
+                  Image.file(
+                    imageFile,
+                    width: 400,
+                    height: 400,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      //border:Border.all(color: Colors.grey,width: 5),
+                      borderRadius: BorderRadius.circular(25),
+                      color: Colors.grey
                     ),
-                    Text(
-                        chr,
-                        style: TextStyle(
-                            fontSize: 20,
-                            color: Colors.white),
-                        textAlign: TextAlign.right
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(chr,
+                          style: TextStyle(fontSize: 25, color: Colors.black,fontWeight: FontWeight.bold),
+                          textAlign: TextAlign.right),
                     ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    ElevatedButton(
-                        onPressed: (){
-                          Clipboard.setData(ClipboardData(text: chr));
-                        },
-                        child: Text("Copy",style: TextStyle(
-                            color: Colors.white
-                        )
-                        )
-                    ),
-                    SizedBox(width: 10),
-                    ElevatedButton(
-                    onPressed:() async {
-    var tempDir = await getTemporaryDirectory();
-    download(Dio(), "http://$IP:5000/return_PDF", tempDir.path + "/aaa.pdf");},
-                        child: Text("PDF",style: TextStyle(
-                            color: Colors.white
-                        )
-                        )
-                    ),
-                  ],
-                )]),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                          style: NotCopy
+                              ? ButtonStyle(
+                                  backgroundColor:
+                                      MaterialStatePropertyAll(Colors.grey))
+                              : ButtonStyle(
+                                  backgroundColor:
+                                      MaterialStatePropertyAll(Colors.blue)),
+                          onPressed: NotCopy?(){}:() {
+                            Clipboard.setData(ClipboardData(text: chr));
+                          },
+                          child: Text("Copy",
+                              style: TextStyle(color: Colors.white))),
+                      SizedBox(width: 10),
+                      ElevatedButton(
+                        style: NotCopy
+                              ? ButtonStyle(
+                                  backgroundColor:
+                                      MaterialStatePropertyAll(Colors.grey))
+                              : ButtonStyle(
+                                  backgroundColor:
+                                      MaterialStatePropertyAll(Colors.blue)),
+                          onPressed: NotCopy?(){}:() async {
+                            var tempDir = await getTemporaryDirectory();
+                            download(Dio(), "http://$IP:5000/return_PDF",
+                                tempDir.path + "/aaa.pdf");
+                          },
+                          child: Text("PDF",
+                              style: TextStyle(color: Colors.white))),
+                    ],
+                  )
+                ]),
               ),
-        ),
-      ),
-    ));
+            ),
+          ),
+        ));
   }
 
   double progress = 0;
@@ -133,17 +156,22 @@ class _imageViewState extends State<imageView> {
 
   // Show the progress status to the user.
   String progressString = 'File has not been downloaded yet.';
+
   void updateProgress(done, total) {
     progress = done / total;
     setState(() {
       if (progress >= 1) {
-        progressString = '✅ File has finished downloading. Try opening the file.';
+        progressString =
+            '✅ File has finished downloading. Try opening the file.';
         didDownloadPDF = true;
       } else {
-        progressString = 'Download progress: ' + (progress * 100).toStringAsFixed(0) + '% done.';
+        progressString = 'Download progress: ' +
+            (progress * 100).toStringAsFixed(0) +
+            '% done.';
       }
     });
   }
+
   Future download(Dio dio, String url, String savePath) async {
     try {
       Response response = await dio.get(
@@ -152,8 +180,9 @@ class _imageViewState extends State<imageView> {
         options: Options(
             responseType: ResponseType.bytes,
             followRedirects: false,
-            validateStatus: (status) { return status! < 500; }
-        ),
+            validateStatus: (status) {
+              return status! < 500;
+            }),
       );
       var file = File(savePath).openSync(mode: FileMode.write);
       print(savePath);
@@ -167,5 +196,4 @@ class _imageViewState extends State<imageView> {
       print(e);
     }
   }
-
 }
